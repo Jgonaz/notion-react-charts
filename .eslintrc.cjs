f10085987c606ef61cf3099be1bcf7be90ec8d2f
Jorge González <jorge.gonzalez@nyxell.com>
2023-11-14 12:29:48 +0100

    Fix Standard, añadir Sass, añadir button Datos

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, node: true, es2020: true },
   extends: [
+    'standard',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -12,9 +13,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ]
+    // TODO Al instalar standard como devDevendency ignora por alguna razón (bug) las reglas de eslint.
+    // 'prefer-const': 'off', // Evita el error: 'let' is never reassigned. Use 'const' instead. (prefer-const) standard(prefer-const)
+    'multiline-ternary': 'off', // Evita el error: Expected newline between consequent and alternate of ternary expression. (multiline-ternary) standard(multiline-ternary)
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import './App.css'
-import { useEffect } from 'react'
-import { getCategorias, getGastos, getIngresos } from './services/api.js'
+import { useState } from 'react'
+// import { getCategorias, getGastos, getIngresos } from './services/api.js'
+import './styles/styles.scss'
+import databaseIcon from './assets/icons/database-icon.svg'
+import Charts from './components/Charts.jsx'
 
 function App () {
-  useEffect(() => {
-    getCategorias()
-    getGastos()
-    getIngresos()
-  }, [])
+  const [data, setData] = useState(undefined)
+
+  const downloadData = () => {
+    console.log('Descargando datos...')
+    setData(true) // Prueba
+  }
 
   return (
     <>
-      <p>Hello, world!</p>
+      <div className='flex-center flex-column'>
+        <img src={databaseIcon} alt='Database icon' width={320} height={320} />
+        <div>
+          {data ? (
+            <Charts data={data} />
+          ) : (
+            <div className='flex-center flex-column'>
+              <button className='main-btn' type='button' onClick={downloadData}>
+                <span>Descargar datos</span>
+              </button>
+              <p>Pulsa descargar para mostrar la gráfica.</p>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   )
 }
