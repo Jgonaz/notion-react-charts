b39dffff5b9e1a7484e3c62cb5b1bad861d4dd9c
Jorge González <jgonaz.dev@gmail.com>
2023-11-21 23:52:47 +0100

    Update packages, correcciones estilos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './styles/styles.scss'
-
 import { useContext } from 'react'
 import { NotionDataContext } from './contexts/NotionDataContext.jsx'
-
 import Charts from './components/PieChart.jsx'
 import Loading from './components/Loading.jsx'
 import { groupByCategories } from './utils/dataMapping.js'
-import databaseIcon from './assets/icons/database-icon.svg'
 
 function App () {
-  // Usa el hook useContext para acceder al contexto
   const { loading, notionData, downloadData, dateInformation, totalAmount } = useContext(NotionDataContext)
 
   return (
@@ -21,12 +17,10 @@ function App () {
             <>
               {!notionData && (
                 <div>
-                  <img src={databaseIcon} alt='Database icon' width={320} height={320} />
                   <div className='flex-center flex-column'>
                     <button className='main-btn' type='button' onClick={downloadData}>
-                      <span>Descargar datos</span>
+                      Mostrar gastos por meses
                     </button>
-                    <p>Pulsa descargar para mostrar la gráfica.</p>
                   </div>
                 </div>
               )}
