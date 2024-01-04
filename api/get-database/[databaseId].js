b2655faa1a7128d53cb35672d2ad5ed1d2381e90
Jorge González <jgonaz.dev@gmail.com>
2024-01-04 17:39:29 +0100

    Cambio en rutas serverless vercel

--- api/get-database/[databaseId].js
+++ api/get-database/[databaseId].js
@@ -3,7 +3,7 @@ import { Client } from '@notionhq/client'
 const notion = new Client({ auth: process.env.VITE_NOTION_API_KEY })
 
 export default async function (req, res) {
-  const { databaseId } = req.query // Asegúrate de obtener los parámetros correctamente
+  const { databaseId } = req.params // Asegúrate de obtener los parámetros correctamente
 
   try {
     const response = await notion.databases.query({
@@ -13,10 +13,8 @@ export default async function (req, res) {
     res.json(response)
   } catch (error) {
     console.error(error)
-    res
-      .status(500)
-      .json({
-        error: `Error al obtener datos de la base de datos con ID: ${databaseId}`
-      })
+    res.status(500).json({
+      error: `Error al obtener datos de la base de datos con ID: ${databaseId}`
+    })
   }
 }
